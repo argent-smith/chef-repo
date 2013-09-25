e47548ce4116d16e58c54f952133962b0daf2ad4
Pavel Argentov <argentoff@gmail.com>
2013-09-25 17:06:16 +0400

    JS development tools

---
+++ cookbooks/js_development_tools/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Pavel Argentov"
+maintainer_email "argentoff@gmail.com"
+license          "Apache 2.0"
+description      "Installs/Configures JS development tools"
+
+version          "0.1"
